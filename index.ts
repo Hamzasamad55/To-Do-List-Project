6fb58ee2f385fcd09b4909a25454f72145336805
Hamza Samad <160117284+Hamzasamad55@users.noreply.github.com>
2024-05-09 14:02:33 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let toDoList = [];
+let condition = true;
+
+while (condition) {
+  let addTask = await inquirer.prompt([
+    {
+      name: "toDo",
+      type: "input",
+      message: "What Do You Want To Add In Your To Do List?",
+    },
+
+    {
+      name: "addMore",
+      type: "confirm",
+      message: "Do You Want To Add More?",
+      default: "false",
+    },
+  ]);
+
+  toDoList.push(addTask.toDo);
+  condition = addTask.addMore;
+  console.log(toDoList);
+}
